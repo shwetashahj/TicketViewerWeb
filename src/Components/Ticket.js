a4869bbcd2340faa9c446f1a9057b12c6f5d9aed
Shweta Shah <shwetushah123@gmail.com>
2021-11-28 23:58:56 -0500

    Add ticket details screen

--- src/Components/Ticket.js
+++ src/Components/Ticket.js
@@ -4,12 +4,12 @@ export const Ticket = ({ticket}) => {
     const history = useNavigate();
 
     return (
-        <tr className = "cursor-pointer" onClick={() => history('TicketDetails')}> 
+        <tr className = "cursor-pointer" onClick={() => history('TicketDetails',{state:{ticketId:ticket.id}})}> 
             <th scope="row">{ticket.id}</th>
-            <td>{ticket.subject}</td>
-            <td>{ticket.priority}</td>
-            <td>{ticket.status}</td>
-            <td>{ticket.createdDate}</td>
+            <td>{ticket.subject !== null ?ticket.subject:"-"}</td>
+            <td>{ticket.priority !== null ?ticket.priority:"-"}</td>
+            <td>{ticket.status !== null ?ticket.status:"-"}</td>
+            <td>{ticket.createdDate !== null ?ticket.createdDate:"-"}</td>
         </tr>
     )
 }
--- src/Components/TicketDetails.js
+++ src/Components/TicketDetails.js
@@ -1,7 +1,89 @@
+import React,{useState,useEffect} from 'react';
+import { LoadingSpinner } from "./LoadingSpinner";
+import { useLocation } from "react-router-dom";
+
+
+
 export const TicketDetails = () => {
+
+   
+    const location = useLocation();
+    const id = location.state.ticketId;
+    const [ticket,setTicket] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError]  = useState('');
+
+    useEffect(() => {
+        const getTicketDetail = async () => {
+          fetchTasks()
+        }
+        getTicketDetail()
+      }, [])
+    
+    const fetchTasks = async () => {
+        console.log(id);
+        fetch(`http://localhost:8080/showTicket/${id}`).then((response) => {
+              if(response.ok) return response.json();
+              throw new Error('something went wrong');
+            })
+            .then((response) => {
+                setIsLoading(false);
+                setTicket(response.data.ticket);
+              })
+            .catch((error) => {
+                setIsLoading(false);
+                setError(error.message);
+            });
+      }
+      if(error) return <h1>{error}</h1>
+
     return(
-        <div>
-            <h1>In Details</h1>
+        isLoading ? <LoadingSpinner/> :
+        <div style={{display:"flex", flexDirection:"column"}}>
+             <h4>Ticket Details</h4>
+             <table  className = "table header-fixed" style={{width:"1000px"}}>
+                <thead>
+                </thead>
+                <tbody>
+                    <tr className = "cursor-pointer" > 
+                        <td>ID:</td>
+                        <td>{ticket.id}</td>
+                    </tr>
+                    <tr className = "cursor-pointer" > 
+                        <td>SUBJECT:</td>
+                        <td>{ticket.subject}</td>
+                    </tr>
+                    <tr className = "cursor-pointer" > 
+                        <td>DESCRIPTION:</td>
+                        <td>{ticket.description !== null ?ticket.description:"-"}</td>
+                    </tr>
+                    <tr className = "cursor-pointer" > 
+                        <td>PRIORITY:</td>
+                        <td>{ticket.priority !== null ?ticket.priority:"-"}</td>
+                    </tr>
+                    <tr className = "cursor-pointer" > 
+                        <td>STATUS:</td>
+                        <td>{ticket.status !== null ?ticket.status:"-"}</td>
+                    </tr>
+                    <tr className = "cursor-pointer" > 
+                        <td>TYPE:</td>
+                        <td>{ticket.type !== null ?ticket.type:"-"}</td>
+                    </tr>
+                    <tr className = "cursor-pointer" > 
+                        <td>EMAIL IN CC:</td>
+                        <td>{ticket.emailsInCC ? ticket.emailsInCC.map(item => {
+                            return (
+                            <h5>item</h5>
+                            );
+                        }):"-"}</td>
+                    </tr>
+                    <tr className = "cursor-pointer" > 
+                        <td>REQUESTER ID:</td>
+                        <td>{ticket.requesterId !== null ?ticket.requesterId:"-"}</td>
+                    </tr>
+                    
+                 </tbody>
+            </table>
         </div>
 
     )
